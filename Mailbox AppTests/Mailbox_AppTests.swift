83ba4ff37a74431788f9069db6f97cd908ca220d
Amritha Prasad <amrithap27@gmail.com>
2015-05-23 17:11:44 -0700

    Initial Commit

---
+++ Mailbox AppTests/Mailbox_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Mailbox_AppTests.swift
+//  Mailbox AppTests
+//
+//  Created by Amritha Prasad on 5/23/15.
+//  Copyright (c) 2015 Amritha Prasad. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Mailbox_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
